d2af4946ab3c68e133d02b0cec0667cf976ce50f
Samuel Espinosa <saespmar@gmail.com>
2019-04-27 20:25:58 +0200

    Basic functionality showing the response in the console

---
+++ web/js/request.js
@@ -0,0 +1,45 @@
+async function sendRequest() {
+
+    // Get the URL
+    var url_req = document.getElementById('url-req');
+    var url = url_req.value;
+
+    // Check if the URL parameter is filled
+    if (url == "" || url == null){
+        url_req.style.borderColor = "red";
+        alert("URL can't be empty");
+    } else {
+
+        // Reset the border color
+        url_req.style.borderColor = "#a9a9a9";
+        
+        // Get the method
+        var op_req = document.getElementById("op-req");
+        var operation = op_req.options[op_req.selectedIndex].text;
+
+        // GET and HEAD can't have body
+        if (operation == "GET" || operation == "HEAD") {
+            var settings = {
+                method: operation
+            }
+        } else {
+            var body_req = document.getElementById("body-req");
+            var body_value = body_req.value;
+    
+            var settings = {
+                method: operation,
+                body: body_value
+            }
+        }
+
+        // Async request
+        var result = await fetch(url, settings)
+        .then((response) => {
+            return response.json();
+        }).then((data) => {
+            return data;
+        }); 
+        
+        console.log(result);
+    }
+}
