8a59140c6664e1feb3dc16f3437b832093e842f5
Samuel Espinosa <saespmar@gmail.com>
2019-04-28 17:07:31 +0200

    Show a warning if the body is filled with the methods GET or HEAD

--- web/js/request.js
+++ web/js/request.js
@@ -4,13 +4,20 @@ async function sendRequest() {
     var url_req = document.getElementById('url-req');
     var url = url_req.value;
 
+    // Get the body
+    var body_req = document.getElementById("body-req");
+    var body_value = body_req.value;
+
     // Check if the URL parameter is filled
     if (url == "" || url == null){
         url_req.style.borderColor = "red";
         document.getElementById('error-req').innerHTML = "URL can't be empty";
+
+        // Reset the errors related with filling the body textarea
+        body_req.style.borderColor = "#a9a9a9";
     } else {
 
-        // Reset the errors
+        // Reset the errors related with empty URL
         url_req.style.borderColor = "#a9a9a9";
         document.getElementById('error-req').innerHTML = "";
         
@@ -18,19 +25,28 @@ async function sendRequest() {
         var op_req = document.getElementById("op-req");
         var operation = op_req.options[op_req.selectedIndex].text;
 
-        // GET and HEAD can't have body
         if (operation == "GET" || operation == "HEAD") {
             var settings = {
                 method: operation
             }
+
+            // Show error if GET or HEAD methods have body
+            if (body_value != "" && body_value != null){
+                document.getElementById('error-req').innerHTML = "Body data omitted because you selected method " + operation;
+                body_req.style.borderColor = "red";
+            } else {
+
+                // Reset the errors related with filling the body textarea
+                body_req.style.borderColor = "#a9a9a9";
+            }
         } else {
-            var body_req = document.getElementById("body-req");
-            var body_value = body_req.value;
-    
             var settings = {
                 method: operation,
                 body: body_value
             }
+
+            // Reset the errors related with filling the body textarea
+            body_req.style.borderColor = "#a9a9a9";
         }
 
         // Async request
